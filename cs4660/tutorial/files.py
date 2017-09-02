34567736fc823bf1900c426915812bc37040de64
ellipsclamation <seppcness@gmail.com>
2017-09-02 14:49:04 -0700

    completed exercise 2

--- cs4660/tutorial/files.py
+++ cs4660/tutorial/files.py
@@ -8,31 +8,40 @@ class SimpleFile(object):
         TODO: reads the file by path and parse content into two
         dimension array (numbers)
         """
+        with open(file_path) as f:
+            for line in f:
+                self.numbers.append(int(i) for i in line.split())
 
     def get_mean(self, line_number):
         """
         get_mean retrieves the mean value of the list by line_number (starts
         with zero)
         """
-        pass
+        sum = 0
+        count = 0
+        for i in self.numbers[line_number]:
+            sum += i
+            count += 1
+
+        return sum / count
 
     def get_max(self, line_number):
         """
         get_max retrieves the maximum value of the list by line_number (starts
         with zero)
         """
-        pass
+        return max(self.numbers[line_number])
 
     def get_min(self, line_number):
         """
         get_min retrieves the minimum value of the list by line_number (starts
         with zero)
         """
-        pass
+        return min(self.numbers[line_number])
 
     def get_sum(self, line_number):
         """
         get_sum retrieves the sumation of the list by line_number (starts with
         zero)
         """
-        pass
+        return sum(self.numbers[line_number])
--- cs4660/tutorial/lists.py
+++ cs4660/tutorial/lists.py
@@ -2,20 +2,20 @@
 
 def get_first_item(li):
     """Return the first item from the list"""
-    pass
+    return li[0]
 
 def get_last_item(li):
     """Return the last item from the list"""
-    pass
+    return li[-1]
 
 def get_second_and_third_items(li):
     """Return second and third item from the list"""
-    pass
+    return li[1:3]
 
 def get_sum(li):
     """Return the sum of the list items"""
-    pass
+    return sum(li)
 
 def get_avg(li):
     """Returns the average of the list items"""
-    pass
+    return get_sum(li) / len(li)
